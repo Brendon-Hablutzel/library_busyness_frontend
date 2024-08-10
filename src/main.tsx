88a7674627c3ba172e9784ea271754fd2d9f25bc
Brendon Hablutzel <booklvrbren@gmail.com>
2024-08-10 17:25:35 -0400

    remove react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import App from './App.tsx'
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home.tsx";
-import ErrorPage from "./ErrorPage.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/library_busyness_frontend",
-    element: <Home />,
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Home />
   </React.StrictMode>
 );
